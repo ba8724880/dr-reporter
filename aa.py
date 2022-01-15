86e91111c3f2fb868b195c184aff09967645ecd5
Babar Ali <59133683+ba8724880@users.noreply.github.com>
2022-01-15 17:12:11 +0500

    Update aa.py

--- aa.py
+++ aa.py
@@ -80,11 +80,11 @@ print '\033[1;32m[+]19 Reports Send Successfully '
 time.sleep(0.9)
 print '\033[1;32m[+]20 Reports Send Successfully '
 #fuck you copy waly
-time.sleep(3)
+time.sleep(0.1)
 print '\033[1;32m[+]\033[1;41m\033[1;33m20 Reports Has Been Send Successfully               \033[1;0m   '  
-time.sleep(4)
+time.sleep(3)
 print '\033[1;32m[+]\033[1;41m\033[1;31m  500 Reports in Progress                           \033[1;0m'  
-time.sleep(4)
+time.sleep(5)
 print '\033[1;32m[+]\033[1;41m\033[1;31m        Please Wait                                 \033[1;0m'  
 time.sleep(15)
 print '\033[1;32m[+]\033[1;41m\033[1;33m500 Reports Has Been Send Successfully              \033[1;0m '
